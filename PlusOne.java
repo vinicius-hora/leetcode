248e97c91ad909c3f7560f72812be369db41d2ec
vinicius-hora <vinicius_hora@live.com>
2024-08-06 11:42:13 -0300

    plusOne

--- PlusOne.java
+++ PlusOne.java
@@ -36,16 +36,41 @@ Constraints:
 digits does not contain any leading 0's.
  */
 
+import java.util.ArrayList;
 
 public class PlusOne {
     public static void main(String[] args) {
+
+        PlusOne plusOne = new PlusOne();
+        int[]  digits = {4,3,2,1};
+
+        for(int i = 0; i < digits.length; i++){
+            System.out.println(plusOne.plusOne(digits)[i]);
+        }
+        
         
     }
 
     public int[] plusOne(int[] digits) {
-        
 
-        return null;
+        int i = 0;
+        int[] novaMatriz = new int[digits.length + 1];
+        int numeroAtual = 0;
+
+
+        for(i = digits.length - 1; i >= 0; i--){
+            numeroAtual = digits[i];
+            if(numeroAtual + 1 != 10){
+                digits[i]  = numeroAtual + 1;
+                return digits;
+            }
+            digits[i] = 0;
+            
+
+        }
+        novaMatriz[0] = 1;
+
+        return novaMatriz;
     }
     
 }
